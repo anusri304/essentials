5f3cc86380a56c1e12566300d3a0141503137e22
Anandhi <anu.keepsmiling@gmail.com>
2020-07-03 22:15:54 +0200

    commit for register activity

--- app/src/main/java/com/example/essentials/MainActivity.java
+++ app/src/main/java/com/example/essentials/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.essentials;
 
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
-
-import android.app.ProgressDialog;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import androidx.databinding.DataBindingUtil;
 
 import com.example.essentials.databinding.ActivityRegisterBinding;
 import com.example.essentials.service.CustomerService;
@@ -17,6 +20,8 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
@@ -24,10 +29,6 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
-import androidx.databinding.DataBindingUtil;
-
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -42,56 +43,222 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         activityRegisterBinding = DataBindingUtil.setContentView(this, R.layout.activity_register);
         activityRegisterBinding.registerButton.setOnClickListener(this);
         setTitle("Register");
+        initLayout();
         //TODO: validate email address, all fields are captured
         // add newsletters, first and last name
         // network connection and rotation
     }
 
+    public static boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
 
-    private void getCustomerByEmail(View view) {
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(interceptor)
-                .retryOnConnectionFailure(true)
-                .connectTimeout(15, TimeUnit.SECONDS)
-                .build();
-// The App will not crash for malformed JSON.
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
-        if (retrofit == null) {
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(ApplicationConstants.BASE_URL)
-                    .client(client)
-                    .addConverterFactory(GsonConverterFactory.create(gson))
-                    .build();
-        }
-        CustomerService customerService = retrofit.create(CustomerService.class);
-        Call<RegisterTransportBean> call = customerService.registerCustomer(activityRegisterBinding.editTextEmailAddress.getText().toString(), activityRegisterBinding.editTextName.getText().toString(), activityRegisterBinding.editTextName.getText().toString(), activityRegisterBinding.editTextMobileNo.getText().toString(), activityRegisterBinding.editTextPassword.getText().toString());
-
-
-        activityRegisterBinding.progressBar.setVisibility(View.VISIBLE);
-
-        call.enqueue(new Callback<RegisterTransportBean>() {
+    private void initLayout() {
+        activityRegisterBinding.editTextName.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onResponse(Call<RegisterTransportBean> call, Response<RegisterTransportBean> response) {
-                activityRegisterBinding.progressBar.setVisibility(View.INVISIBLE);
-                RegisterTransportBean registerTransportBean = response.body();
-                Log.i(TAG, "onResponse: " + registerTransportBean.getMessage());
-                if (registerTransportBean.getMessage() != null && registerTransportBean.getMessage().contains(ApplicationConstants.MODIFIED_SUCCESS)) {
-                    showMessage(ApplicationConstants.REGISTER_SUCCESS);
-                } else {
-                    showMessage(ApplicationConstants.REGISTER_FAILURE);
-                }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
             }
 
             @Override
-            public void onFailure(Call<RegisterTransportBean> call, Throwable throwable) {
-                activityRegisterBinding.progressBar.setVisibility(View.INVISIBLE);
-                Log.e(this.getClass().getName(), throwable.toString());
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 0)
+                    activityRegisterBinding.textInputLayoutName.setError(ApplicationConstants.NAME_ERROR_MESSAGE);
+                else
+                    activityRegisterBinding.textInputLayoutName.setError(null);
+
             }
         });
+
+
+        activityRegisterBinding.editTextEmailAddress.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 0)
+                    activityRegisterBinding.textInputLayoutEmailAdds.setError(ApplicationConstants.EMAIL_ADDRESS_ERROR_MESSAGE);
+                else if (!isValidEmail(s.toString())) {
+                    activityRegisterBinding.textInputLayoutEmailAdds.setError(ApplicationConstants.EMAIL_ADDRESS_FORMAT_ERROR_MESSAGE);
+                } else
+                    activityRegisterBinding.textInputLayoutEmailAdds.setError(null);
+
+            }
+        });
+
+        activityRegisterBinding.editTextMobileNo.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 0)
+                    activityRegisterBinding.textInputLayoutMobileNo.setError(ApplicationConstants.TELEPHONE_ERROR_MESSAGE);
+                else
+                    activityRegisterBinding.textInputLayoutMobileNo.setError(null);
+
+            }
+        });
+
+        activityRegisterBinding.editTextPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 0)
+                    activityRegisterBinding.textInputLayoutPassword.setError(ApplicationConstants.PASSWORD_ERROR_MESSAGE);
+                else
+                    activityRegisterBinding.textInputLayoutPassword.setError(null);
+
+            }
+        });
+
+        activityRegisterBinding.editTextConfirmPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 0)
+                    activityRegisterBinding.textInputLayoutConfirmPwd.setError(ApplicationConstants.CONFIRM_PASSWORD_ERROR_MESSAGE);
+                else
+                    activityRegisterBinding.textInputLayoutConfirmPwd.setError(null);
+
+            }
+        });
+
+    }
+
+
+    private void getCustomerByEmail(View view) {
+        if (validateFields()) {
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    .retryOnConnectionFailure(true)
+                    .connectTimeout(15, TimeUnit.SECONDS)
+                    .build();
+// The App will not crash for malformed JSON.
+            Gson gson = new GsonBuilder()
+                    .setLenient()
+                    .create();
+            if (retrofit == null) {
+                retrofit = new Retrofit.Builder()
+                        .baseUrl(ApplicationConstants.BASE_URL)
+                        .client(client)
+                        .addConverterFactory(GsonConverterFactory.create(gson))
+                        .build();
+            }
+            CustomerService customerService = retrofit.create(CustomerService.class);
+            Call<RegisterTransportBean> call = customerService.registerCustomer(activityRegisterBinding.editTextEmailAddress.getText().toString(), activityRegisterBinding.editTextName.getText().toString(), activityRegisterBinding.editTextName.getText().toString(), activityRegisterBinding.editTextMobileNo.getText().toString(), activityRegisterBinding.editTextPassword.getText().toString());
+
+
+            activityRegisterBinding.progressBar.setVisibility(View.VISIBLE);
+
+            call.enqueue(new Callback<RegisterTransportBean>() {
+                @Override
+                public void onResponse(Call<RegisterTransportBean> call, Response<RegisterTransportBean> response) {
+                    activityRegisterBinding.progressBar.setVisibility(View.INVISIBLE);
+                    RegisterTransportBean registerTransportBean = response.body();
+                    Log.i(TAG, "onResponse: " + registerTransportBean.getMessage());
+                    if (registerTransportBean.getMessage() != null && registerTransportBean.getMessage().contains(ApplicationConstants.MODIFIED_SUCCESS)) {
+                        showMessage(ApplicationConstants.REGISTER_SUCCESS);
+                    } else {
+                        showMessage(ApplicationConstants.REGISTER_FAILURE);
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<RegisterTransportBean> call, Throwable throwable) {
+                    activityRegisterBinding.progressBar.setVisibility(View.INVISIBLE);
+                    Log.e(this.getClass().getName(), throwable.toString());
+                }
+            });
+        }
+    }
+
+    private boolean validateFields() {
+        boolean isValid = true;
+        if (activityRegisterBinding.editTextName.getText().toString().isEmpty()) {
+            isValid = false;
+            activityRegisterBinding.textInputLayoutName.setError(ApplicationConstants.NAME_ERROR_MESSAGE);
+        } else {
+            activityRegisterBinding.textInputLayoutName.setError(null);
+        }
+
+        if (activityRegisterBinding.editTextEmailAddress.getText().toString().isEmpty()) {
+            isValid = false;
+            activityRegisterBinding.textInputLayoutEmailAdds.setError(ApplicationConstants.EMAIL_ADDRESS_ERROR_MESSAGE);
+        } else {
+            activityRegisterBinding.textInputLayoutEmailAdds.setError(null);
+        }
+
+        if (activityRegisterBinding.editTextMobileNo.getText().toString().isEmpty()) {
+            isValid = false;
+            activityRegisterBinding.textInputLayoutMobileNo.setError(ApplicationConstants.TELEPHONE_ERROR_MESSAGE);
+        } else {
+            activityRegisterBinding.textInputLayoutMobileNo.setError(null);
+        }
+
+        if (activityRegisterBinding.editTextPassword.getText().toString().isEmpty()) {
+            isValid = false;
+            activityRegisterBinding.textInputLayoutPassword.setError(ApplicationConstants.PASSWORD_ERROR_MESSAGE);
+        } else {
+            activityRegisterBinding.textInputLayoutPassword.setError(null);
+        }
+
+        if (activityRegisterBinding.editTextConfirmPassword.getText().toString().isEmpty()) {
+            isValid = false;
+            activityRegisterBinding.textInputLayoutConfirmPwd.setError(ApplicationConstants.CONFIRM_PASSWORD_ERROR_MESSAGE);
+        } else {
+            activityRegisterBinding.textInputLayoutConfirmPwd.setError(null);
+        }
+
+
+        if (!activityRegisterBinding.editTextConfirmPassword.getText().toString().equals(activityRegisterBinding.editTextPassword.getText().toString())) {
+            activityRegisterBinding.textInputLayoutConfirmPwd.setError("Password Do Not Match");
+            isValid = false;
+        }
+        else {
+            activityRegisterBinding.textInputLayoutConfirmPwd.setError(null);
+        }
+        return isValid;
     }
 
     @Override
--- app/src/main/java/com/example/essentials/utils/ApplicationConstants.java
+++ app/src/main/java/com/example/essentials/utils/ApplicationConstants.java
@@ -6,6 +6,12 @@ public class ApplicationConstants {
   public static final String MODIFIED_SUCCESS = "successfully modified";
   public static final String REGISTER_SUCCESS = "Customer registered successfully";
   public static final String REGISTER_FAILURE = "This email address is already registered";
+  public static final String NAME_ERROR_MESSAGE = "Please enter your name";
+  public static final String EMAIL_ADDRESS_ERROR_MESSAGE = "Please enter your email address";
+  public static final String TELEPHONE_ERROR_MESSAGE  = "Please enter your telephone number";
+  public static final String PASSWORD_ERROR_MESSAGE = "Please enter your password";
+  public static final String CONFIRM_PASSWORD_ERROR_MESSAGE = "Please confirm your password";
 
   public static final String LOGIN ="LOGIN" ;
+  public static final String EMAIL_ADDRESS_FORMAT_ERROR_MESSAGE = "Please enter a valid email address";
 }
