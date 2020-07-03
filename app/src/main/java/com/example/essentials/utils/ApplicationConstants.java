6c86f40d4cdea1965552c6fbc0e1bee66371a715
Anandhi <anu.keepsmiling@gmail.com>
2020-07-03 12:36:47 +0200

    commit for register activity

--- app/src/main/java/com/example/essentials/utils/ApplicationConstants.java
+++ app/src/main/java/com/example/essentials/utils/ApplicationConstants.java
@@ -2,7 +2,7 @@ package com.example.essentials.utils;
 
 public class ApplicationConstants {
   //  public static final String BASE_URL = "http://10.0.2.2/Opencart/";
-    public static final String BASE_URL = "http://10.0.0.5/Opencart/";
+    public static final String BASE_URL = "http://10.0.0.7/Opencart/";
   public static final String MODIFIED_SUCCESS = "successfully modified";
   public static final String REGISTER_SUCCESS = "Customer registered successfully";
   public static final String REGISTER_FAILURE = "This email address is already registered";
