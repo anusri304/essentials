335ef03a68ce24fae7ca88a6f80cfabaf61a85b5
Anandhi <anu.keepsmiling@gmail.com>
2020-07-12 21:48:01 +0200

    Navigation drawer for launch activity

--- app/src/main/java/com/example/essentials/activity/ProductActivity.java
+++ app/src/main/java/com/example/essentials/activity/ProductActivity.java
@@ -1,25 +1,80 @@
 package com.example.essentials.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.databinding.DataBindingUtil;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import com.example.essentials.R;
-import com.example.essentials.databinding.ActivityLoginBinding;
-import com.example.essentials.databinding.ActivityProductBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class ProductActivity extends AppCompatActivity {
-    ActivityProductBinding activityProductBinding;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        activityProductBinding = DataBindingUtil.setContentView(this, R.layout.activity_product);
+        setContentView(R.layout.activity_product);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        configureToolbar();
+        configureNavigationDrawer();
 
-        setSupportActionBar(activityProductBinding.toolbar);
-        activityProductBinding.toolbarTitle.setText(R.string.app_name);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
+    private void configureToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.topAppBar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        // actionbar.setHomeAsUpIndicator(R.drawable.ic_action_menu_white);
+        actionbar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void configureNavigationDrawer() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navView = (NavigationView) findViewById(R.id.navigation);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                Fragment f = null;
+                int itemId = menuItem.getItemId();
+                if (itemId == R.id.homeIcon) {
+                    //  f = new RefreshFragment();
+                    Log.d("Product Activity","Inside home");
+                    Toast.makeText(ProductActivity.this, "Hello home", Toast.LENGTH_LONG).show();
+                } else if (itemId == R.id.login) {
+                    Log.d("Product Activity","Inside login");
+                    Toast.makeText(ProductActivity.this, "Hello login", Toast.LENGTH_LONG).show();
+                }
+//                if (f != null) {
+//                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//                    transaction.replace(R.id.frame, f);
+//                    transaction.commit();
+//                    drawerLayout.closeDrawers();
+//                    return true;
+//                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        switch (itemId) {
+            // Android home
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            // manage other entries if you have it ...
+        }
+        return true;
+    }
 }
