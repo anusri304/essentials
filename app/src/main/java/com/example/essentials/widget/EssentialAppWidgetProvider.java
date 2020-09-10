587678cc846b27f236a1c6f477e57d843a35dcfb
Anandhi <anu.keepsmiling@gmail.com>
2020-09-10 20:31:27 +0200

    Added code for displaying the App widget

--- app/src/main/java/com/example/essentials/widget/EssentialAppWidgetProvider.java
+++ app/src/main/java/com/example/essentials/widget/EssentialAppWidgetProvider.java
@@ -5,8 +5,10 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.widget.RemoteViews;
 
+import com.bumptech.glide.Glide;
 import com.example.essentials.R;
 import com.example.essentials.activity.ProductActivity;
 import com.example.essentials.service.DisplayPromotionProductService;
@@ -14,14 +16,26 @@ import com.example.essentials.service.DisplayPromotionProductService;
 public class EssentialAppWidgetProvider extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                String ingredients, int appWidgetId) {
+                                String ingredients, String imagePath,String price, int appWidgetId) {
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.essentials_app_widget);
-        views.setTextViewText(R.id.appwidget_text, ingredients);
+        views.setTextViewText(R.id.appwidget_productName, ingredients);
 
+        try {
+            Bitmap bitmap = Glide.with(context)
+                    .asBitmap()
+                    .load(imagePath)
+                    .submit(820, 430)
+                    .get();
+
+            views.setImageViewBitmap(R.id.productImageView, bitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+      //  views.setTextViewText(R.id.appwidget_productPrice, price);
         Intent intent = new Intent(context, ProductActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+        views.setOnClickPendingIntent(R.id.appwidget_productName, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -33,10 +47,10 @@ public class EssentialAppWidgetProvider extends AppWidgetProvider {
         DisplayPromotionProductService.startActionDisplayPromotionProductWidgets(context);
     }
 
-    public static void updateProductWidget(Context context, AppWidgetManager appWidgetManager, String ingredients, int[] appWidgetIds) {
+    public static void updateProductWidget(Context context, AppWidgetManager appWidgetManager, String ingredients, String imagePath,String price, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, ingredients, appWidgetId);
+            updateAppWidget(context, appWidgetManager, ingredients, imagePath, price, appWidgetId);
         }
     }
 
